c5bba70591a01c145a26421e2fbc176a377ad7ae
Madhura <madura12a@gmail.com>
2020-01-14 07:37:06 +0530

    Constraints

--- HBO/ViewController.swift
+++ HBO/ViewController.swift
@@ -13,8 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //setBackground();
     }
 
+    func setBackground(){
+        view.addSubview(BackgroundImageView)
+        BackgroundImageView.translatesAutoresizingMaskIntoConstraints = false;
+        BackgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        BackgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        BackgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        BackgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        BackgroundImageView.image = UIImage(named: "4cbb96f6ec860f96ab3351a9a1612d5a")
+    }
 
+    let BackgroundImageView = UIImageView()
+    @IBOutlet weak var txtFName: UITextField!
+    @IBOutlet weak var txtLName: UITextField!
+    @IBOutlet weak var txtEmailAddress: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtConfirmPassword: UITextField!
+    
+    
 }
 
